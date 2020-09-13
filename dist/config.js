6ad98181e08b01ee9ed514c5539cdc1a41a090c4
freddneos <fredd@neosdev.com.br>
2020-09-13 01:44:35 +0100

    feat: avoiding build on cloud and push dist files to heroku and github

---
+++ dist/config.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SERVER_PORT = void 0;
+exports.SERVER_PORT = parseInt(process.env.SERVER_PORT || process.env.PORT || "3000");
+//# sourceMappingURL=config.js.map
